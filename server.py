6363c0c1a9724e77f4a506e53a6b1ded6f114b29
beckhamer <ft1983@gmail.com>
2014-01-19 14:36:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -13,19 +13,15 @@ print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
 s.listen(5)                 # Now wait for client connection.
 
-# @comment Might want to change the variable name, 'y', to something more descriptive
-y="""HTTP/1.0 200 OK
-Content-Type: text/html
-
-<html>
-<h1>Hello, world</h1> this is beckhamer's Web server.
-</html>"""
-
-
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send(y)
+    # send a response
+    c.send('HTTP/1.0 200 OK\r\n')
+    c.send('Content-type: text/html\r\n')
+    c.send('\r\n')
+    c.send('<h1>Hello, world.</h1>')
+    c.send('this is beckhamer's Web server.')
     c.close()
