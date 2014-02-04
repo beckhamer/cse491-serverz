e841eedf2619c1012402df06aba741a2ea18f1cd
Tao Feng <fengtao@arctic.cse.msu.edu>
2014-02-04 00:36:07 -0500

    split to different functions

--- server.py
+++ server.py
@@ -26,65 +26,119 @@ def handle_connection(conn):
     message = conn.recv(1000)
     # print message
     request_method =  message.splitlines()[0].split()[0]
-    local_path  = message.splitlines()[0].split()[1]
-    host_url = "http://arctic.cse.msu.edu"
-    request_url = host_url+local_path
-    # print request_url
+    request_url = message.splitlines()[0].split()[1]
+    print request_url
     parsed_url = urlparse.urlparse(request_url)
     print parsed_url
     request_path = parsed_url.path
-    request_query = parsed_url.query
-
-#     if local_path.find("?")>0:
-#         request_path = local_path.split("?")[0]
-#         request_data = local_path.split("?")[1]
-#     else:
-#         request_path = local_path
-
+    
     print 'request method:'+request_method
     print 'request path:'+request_path    
-    print 'request query:'+request_query
+    
     # handle different types of HTML requests
     if request_method == 'GET':
         if request_path == '/':
-            html_content = '<form action=\'/submit\' method=\'GET\'>' + \
-			   'First name:<input type=\'text\' name=\'firstname\'>' + \
-			   'Last name:<input type=\'text\' name=\'lastname\'>' + \
-			   '<input type=\'submit\' value=\'Submit\'>' + \
-			   '</form>'
-	elif request_path == '/submit':
-	    parsed_query = urlparse.parse_qs(request_query,True)
-        
-	    firstname = ''.join(parsed_query['firstname'])
-# 	    firstname = request_data.split("&")[0].split("=")[1]
-	    lastname = ''.join(parsed_query['lastname'])
-# 	    lastname = request_data.split("&")[1].split("=")[1]
-            html_content = 'Hello Mr %s %s.' % (firstname, lastname)	    
-        elif request_path == '/content':
-	    html_content = 'This is /content page.'
+	    handle_index(conn, parsed_url)
+	elif request_path == '/content':
+	    handle_content(conn, parsed_url)
 	elif request_path == '/file':
-	    html_content = 'This is /file page.'
-        elif request_path == '/image':
-	    html_content = 'This is /image page.'
-	elif request_path == '/favicon.ico':
-	    html_content = 'This is /favicon.ico page.'
-    elif request_method == 'POST':
-	if request_path == '/':
-	    html_content = 'This is POST request page.'
+	    handle_file(conn, parsed_url)
+	elif request_path == '/image':
+	    handle_image(conn, parsed_url)
+	elif request_path == '/form':
+	    handle_form(conn, parsed_url)
 	elif request_path == '/submit':
-	    html_content = 'Hello Mr Tao Feng.'
-	
-    # send a response
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n')
-    conn.send('\r\n')
-    conn.send('<h1>Hello, world.</h1>')
-    conn.send(html_content)
+	    handle_submit(conn, parsed_url)
+    elif request_method == 'POST':
+        content = message.splitlines()[-1]
+        if request_path == '/':
+	    handle_post_index(conn, content)
+        elif request_path == '/submit':
+            handle_post_submit(conn, content)
     conn.close()
 
-#def handle_form(conn):
-#    message = conn.recv(1000)
-#    request_method = message.splitlines()[0].split[0]
-#    request_path =messgae.splitlines()[0].split()[1]
+def handle_index(conn, parsed_url):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('<h1>Hello, world.</h1>')
+    conn.send("This is beckhamer's page.<br>")
+    conn.send("<a href='/content'>Content page</a><br>")
+    conn.send("<a href='/file'>File page</a><br>")
+    conn.send("<a href='/image'>Image page</a><br>")
+    conn.send("<a href='/form'>Form page</a><br>")
+    conn.send('</html></body>')
+
+def handle_content(conn, parsed_url):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('<h1>Content page</h1>')
+    conn.send("This is beckhamer's content page.<br>")
+    conn.send('</html></body>')
+
+def handle_file(conn, parsed_url):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('<h1>File page</h1>')
+    conn.send("This is beckhamer's file page.<br>")
+    conn.send('</html></body>')
+
+def handle_image(conn, parsed_url):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('<h1>Image page</h1>')
+    conn.send("This is beckhamer's image page.<br>")
+    conn.send('</html></body>')
+
+def handle_form(conn, parsed_url):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send("<form action='/submit' method='GET'>")
+    conn.send("First name:<input type='text' name='firstname'>")
+    conn.send("Last name:<input type='text' name='lastname'>")
+    conn.send("<input type='submit' value='Submit'> </form>")
+    conn.send('</html></body>')
+
+def handle_submit(conn, parsed_url):
+    query = urlparse.parse_qs(parsed_url.query)
+    print 'query:'+query['firstname'][0]+' '+query['lastname'][0]
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('Hello Mr. ')
+    conn.send(query['firstname'][0])
+    conn.send(' ')
+    conn.send(query['lastname'][0])
+    conn.send('.')
+    conn.send('</html></body>')
+
+def handle_post_index(conn, content):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('<h1>Hello, world.</h1>')
+    conn.send("This is beckhamer's page.<br>")
+    conn.send("<a href='/content'>Content page</a><br>")
+    conn.send("<a href='/file'>File page</a><br>")
+    conn.send("<a href='/image'>Image page</a><br>")
+    conn.send("<a href='/form'>Form page</a><br>")
+    conn.send('</html></body>')
+
+def handle_post_submit(conn, content):
+    query = urlparse.parse_qs(content)
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send('<html><body>')
+    conn.send('Hello Mr. ')
+    conn.send(query['firstname'][0])
+    conn.send(' ')
+    conn.send(query['lastname'][0])
+    conn.send('.')
+    conn.send('</html></body>')
+
 if __name__ == '__main__':
     main()
--- test_server.py
+++ test_server.py
@@ -27,29 +27,31 @@ class FakeConnection(object):
 
 # Test a basic GET call.
 
-def test_handle_connection_root():
+def test_handle_connection_index():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
                       '<h1>Hello, world.</h1>' + \
-                      '<form action=\'/submit\' method=\'GET\'>' + \
-                      'First name:<input type=\'text\' name=\'firstname\'>' + \
-                      'Last name:<input type=\'text\' name=\'lastname\'>' + \
-                      '<input type=\'submit\' value=\'Submit\'>' + \
-                      '</form>'
-
+                      "This is beckhamer's page.<br>" + \
+                      "<a href='/content'>Content page</a><br>" + \
+                      "<a href='/file'>File page</a><br>" + \
+                      "<a href='/image'>Image page</a><br>" + \
+                      "<a href='/form'>Form page</a><br>" + \
+                      '</html></body>' 
+    
     server.handle_connection(conn)
 
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
-    
+   
 def test_handle_connection_content():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is /content page.'
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      '<h1>Content page</h1>' + \
+                      "This is beckhamer's content page.<br>" + \
+                      '</html></body>'                       
 
     server.handle_connection(conn)
 
@@ -58,10 +60,11 @@ def test_handle_connection_content():
 def test_handle_connection_file():
     conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is /file page.'
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      '<h1>File page</h1>' + \
+                      "This is beckhamer's file page.<br>" + \
+                      '</html></body>' 
 
     server.handle_connection(conn)
 
@@ -70,22 +73,38 @@ def test_handle_connection_file():
 def test_handle_connection_image():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is /image page.'
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      '<h1>Image page</h1>' + \
+                      "This is beckhamer's image page.<br>" + \
+                      '</html></body>' 
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_form():
+    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      "<form action='/submit' method='GET\'>" + \
+                      "First name:<input type='text' name='firstname'>" + \
+                      "Last name:<input type='text' name='lastname'>" + \
+                      "<input type='submit' value='Submit'> </form>" + \
+                      '</html></body>'
 
     server.handle_connection(conn)
 
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
 def test_handle_connection_GET_submit():
-    conn = FakeConnection("GET /submit?firstname=Tao&lastname=Feng HTTP/1.0\r\n\r\n")
+    conn = FakeConnection("GET /submit?firstname=Tao&lastname=Feng \r\n" + \
+                          "HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'Hello Mr Tao Feng.'
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      "Hello Mr. Tao Feng." + '</html></body>'
 
     server.handle_connection(conn)
 
@@ -94,23 +113,27 @@ def test_handle_connection_GET_submit():
 def test_handle_connection_POST():
     conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
                       '<h1>Hello, world.</h1>' + \
-                      'This is POST request page.'
+                      "This is beckhamer's page.<br>" + \
+                      "<a href='/content'>Content page</a><br>" + \
+                      "<a href='/file'>File page</a><br>" + \
+                      "<a href='/image'>Image page</a><br>" + \
+                      "<a href='/form'>Form page</a><br>" + \
+                      '</html></body>' 
 
     server.handle_connection(conn)
 
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
 def test_handle_connection_POST_submit():
-    conn = FakeConnection("POST /submit HTTP/1.0\r\n\r\n" +\
+    conn = FakeConnection("POST /submit HTTP/1.0\r\n\r\n" + \
 			  "firstname=Tao&lastname=Feng\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'Hello Mr Tao Feng.'
+                      'Content-type: text/html\r\n\r\n' + \
+                      '<html><body>' + \
+                      'Hello Mr. Tao Feng.' + '</html></body>'
 
     server.handle_connection(conn)
 
