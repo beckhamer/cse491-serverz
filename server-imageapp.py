18bbcb57a9b8cae443fb5f697e0e1206e15229cd
beckhamer <ft1983@gmail.com>
2014-04-18 15:49:24 -0400

    Update server-imageapp.py

--- server-imageapp.py
+++ server-imageapp.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python
 import random
 import socket
 import time
-from urlparse import urlparse, parse_qs
-
-## for the new POST request handling
+import urlparse
+from mimetools import Message
 from StringIO import StringIO
 import cgi
-
-## for the templates
-import jinja2
-import os
-
-## for the wsgi app
-# import app
-
-## to run imageapp
-import quixote
+import app
+from wsgiref.validate import validator
 import imageapp
 
 imageapp.setup()
@@ -25,123 +15,70 @@ p = imageapp.create_publisher()
 def make_app():
     return quixote.get_wsgi_app()
 
-
-##
-## HANDLE CONNECTION DEFINITION
-##
 def handle_connection(conn):
-    
-    # Start reading in data from the connection
-    read = conn.recv(1)
-    while read[-4:] != '\r\n\r\n':
-	read += conn.recv(1)
+    message = conn.recv(1)
+    while message[-4:] != '\r\n\r\n':
+        message += conn.recv(1)
 
-    if read[-4:] == '\r\n\r\n':
-	print "READ IS DONE!"
+    request, headers = message.split('\r\n',1)
+    headers = Message(StringIO(headers))
 
-    # Parse headers
-    request, data = read.split('\r\n',1)
-    print read
+    request_method = request.split()[0]
+    request_url = request.split()[1]
 
-    headers = {}
-    for line in data.split('\r\n')[:-2]:
-	k, v = line.split(': ',1)
-	headers[k.lower()] = v
-    
-    # parse path and query string as urlparse object
-    # parsed_url[2] = path, parsed_url[4] = query string
-    parsed_url = urlparse(request.split(' ', )[1])
-    
-    #initialize environ dictionary
+    parsed_url = urlparse.urlparse(request_url)
+    request_path = parsed_url.path
+
     environ = {}
-    
-    environ['PATH_INFO'] = parsed_url[2]
-    environ['QUERY_STRING'] = parsed_url[4]
-    # temporary 'SCRIPT_NAME' entry
-    environ['SCRIPT_NAME'] = ''
-    # temporary 'SERVER_NAME' entry for arctic
-    environ['SERVER_NAME'] = 'arctic.cse.msu.edu'
-    
-    # Handle reading of POST data
+    environ['REQUEST_METHOD'] = request_method
+    environ['PATH_INFO'] = request_path
+    environ['QUERY_STRING'] = parsed_url.query
+    environ['CONTENT_TYPE'] = 'text/html'
+    environ['CONTENT_LENGTH'] = 0
+
     content = ''
-    if request.startswith('POST '):
-	environ['REQUEST_METHOD'] = 'POST'
-	environ['CONTENT_LENGTH'] = headers['content-length']
-	environ['CONTENT_TYPE'] = headers['content-type']
-	# read the remaining data from http request to construct wsgi.input
-	while len(content) < int(headers['content-length']):
-	    content += conn.recv(1)
-    
-    else:
-	environ['REQUEST_METHOD'] = 'GET'
-	environ['CONTENT_LENGTH'] = 0
-    
-    #form = cgi.FieldStorage(fp=StringIO(content), headers=headers, environ=environ)
-   
-    #environ['wsgi.input'] = cgi.FieldStorage(fp=StringIO(content), 
-					      #headers = headers,
-					      #environ = {'REQUEST_METHOD':'POST'} )
-    environ['wsgi.input'] = StringIO(content)
-    print 'wsgi.input made!'
-    
-    
+    if request_method == 'POST':
+        while len(content) < int(headers['Content-Length']):
+            content += conn.recv(1)
+        environ['CONTENT_TYPE'] = headers['Content-Type']
+        environ['CONTENT_LENGTH'] = headers['Content-Length']
+        environ['wsgi.input'] = cgi.FieldStorage(fp=StringIO(content), headers=headers.dict, environ=environ)
+        
     def start_response(status, response_headers):
-	conn.send('HTTP/1.0 %s\r\n' % status)
-	for header in response_headers:
-	    conn.send('%s: %s\r\n' % header)
-	conn.send('\r\n')
+        conn.send('HTTP/1.0 ')
+        conn.send(status)
+        conn.send('\r\n')
+        for pair in response_headers:
+            key, header = pair
+            conn.send(key + ': ' + header + '\r\n')
+        conn.send('\r\n')
 
-    # make the app	
     application = make_app()
-    
+    validator_app = validator(application)
     response_html = application(environ, start_response)
-    for html in response_html:
-        conn.send(html)
-    print 'conn sent!'
-    
-    # close the connection
+    for data in response_html:
+        conn.send(data)
     conn.close()
-    print 'conn closed!'
-
-
-
-
-
-##
-## MAIN FUNCTION DEFINITION
-##
 
 def main():
     s = socket.socket()         # Create a socket object
-    host = socket.getfqdn()     # Get local machine name
+    host = socket.getfqdn() # Get local machine name
     port = random.randint(8000, 9999)
     s.bind((host, port))        # Bind to the port
+
     print 'Starting server on', host, port
     print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+
     s.listen(5)                 # Now wait for client connection.
+
+
     print 'Entering infinite loop; hit CTRL-C to exit'
-    
     while True:
+        # Establish connection with client.
+        c, (client_host, client_port) = s.accept()
+        print 'Got connection from', client_host, client_port
 
-        # Ctrl+C KeyboardInterrupt error handler
-        try:
-            # Establish connection with client.
-            c, (client_host, client_port) = s.accept()
-            print 'Got connection from', client_host, client_port
-            # handle connection to serve page
-            handle_connection(c)
-
-            
-        except (KeyboardInterrupt):
-            print "\r\nEnding server.py ...\r\n"
-            exit(0)
-
-
-
-##
-## RUN MAIN
-##
+        handle_connection(c)
 
 if __name__ == '__main__':
-    main()
-
+   main()
