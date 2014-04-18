db13c6332857586399c0ca835646d82df9251aa0
beckhamer <ft1983@gmail.com>
2014-04-18 02:06:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,45 @@
 from urlparse import urlparse, parse_qs
 import jinja2
 import os
+import cgi
+from StringIO import StringIO
+from wsgiref.simple_server import make_server
 
 def path_render(path, query):
     loader = jinja2.FileSystemLoader('./templates')
     env = jinja2.Environment(loader=loader)
     template = env.get_template(path)
-    return template.render(query).encode('latin-1', 'replace')
+    x = template.render(query).encode('latin-1', 'replace')
+    return str(x)
 
 def simple_app(environ, start_response):
 
     method = environ['REQUEST_METHOD']
     path = environ['PATH_INFO']
-    if method == 'GET':
-	if path == '/':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('index.html', '')
-	elif path == '/content':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('content.html', '')
-	elif path == '/file':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('file.html', '')
-	elif path == '/image':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('image.html', '')
-	elif path == '/form':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('get_form.html', '')
-	elif path == '/form_default':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('form_default.html', '')
-	elif path == '/form_multipart':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    return path_render('form_multipart.html', '')
-	elif path == '/submit':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-	    query = parse_qs(environ['QUERY_STRING'])
-	    query = dict(firstname=query['firstname'][0], lastname=query['lastname'][0])
-	    return path_render('submit.html', query)
-	else:
-            start_response('404 Not Found', [('Content-type', 'text/html')])
-            return path_render('error.html', '')
+    query = ''
 
-    elif method == 'POST':
-	if path == '/':
-	    start_response('200 OK', [('Content-type', 'text/html')])
-            return path_render('index.html', '')
-	elif path == '/form_default':
-            start_response('200 OK', [('Content-type', 'text/html')])
-            return path_render('form_default.html', '')	   
-	elif path == '/form_multipart':
-            start_response('200 OK', [('Content-type', 'text/html')])
-            return path_render('form_multipart.html', '')
-	elif path == '/submit':
-            query = dict(firstname=environ['wsgi.input']['firstname'].value, lastname=environ['wsgi.input']['lastname'].value)
-	    start_response('200 OK', [('Content-type', 'text/html')])
-            return path_render('submit.html', query)
-	else:
-            start_response('404 Not Found', [('Content-type', 'text/html')])
-            return path_render('error.html', '')
+    html_pages = {'/'                 :     'index.html',            \
+                  '/content'          :     'content.html',          \
+                  '/file'             :     'file.html',             \
+                  '/image'            :     'image.html',            \
+                  '/form'             :     'get_form.html',         \
+                  '/form_default'     :     'form_default.html',     \
+                  '/form_multipart'   :     'form_multipart.html',   \
+                  '/submit'           :     'submit.html'            }
+
+    if path in html_pages:
+        start_response('200 OK', [('Content-type', 'text/html')])
+        if path == '/submit':
+            if method == 'GET':
+                query = parse_qs(environ['QUERY_STRING'])
+                query = dict(firstname=query['firstname'][0], lastname=query['lastname'][0])
+            else:
+                query = dict(firstname=environ['wsgi.input']['firstname'].value, lastname=environ['wsgi.input']['lastname'].value)
+        return path_render(html_pages[path], query)
+
+    else:
+        start_response('404 Not Found', [('Content-type', 'text/html')])
+        return path_render('error.html', query)
 
 def make_app():
     return simple_app
-    
