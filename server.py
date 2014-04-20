10bd853685c3b704ee94895505008b22c79df8cb
beckhamer <ft1983@gmail.com>
2014-04-19 22:14:10 -0400

    Update server.py

--- server.py
+++ server.py
@@ -6,31 +6,23 @@ from mimetools import Message
 from StringIO import StringIO
 import cgi
 import app
+from wsgiref.validate import validator
+import quixote
+from quixote.demo.altdemo import create_publisher
+import imageapp
+import argparse
 
-#import quixote
-from quixote.demo import create_publisher
-#from quixote.demo.mini_demo import create_publisher
-#from quixote.demo.altdemo import create_publisher
+def handle_connection(conn, port, app):
+    message = conn.recv(1)
+    while message[-4:] != '\r\n\r\n':
+        message += conn.recv(1)
 
+    request, headers = message.split('\r\n',1)
+    headers = Message(StringIO(headers))
 
-#_the_app = None
-#def make_app():
-#    global _the_app
-#    if _the_app is None:
-#        p = create_publisher()
-#        _the_app = quixote.get_wsgi_app()
-#    return _the_app
+    request_method = request.split()[0]
+    request_url = request.split()[1]
 
-
-
-
-def handle_connection(conn):
-    receive = conn.recv(2000)
-    request_headers, content= receive.split('\r\n\r\n',1)
-    raw_request, raw_headers= request_headers.split('\r\n',1)
-    headers = Message(StringIO(raw_headers))
-    request_method = raw_request.split()[0]
-    request_url = raw_request.split()[1]
     parsed_url = urlparse.urlparse(request_url)
     request_path = parsed_url.path
 
@@ -41,12 +33,23 @@ def handle_connection(conn):
     environ['CONTENT_TYPE'] = 'text/html'
     environ['CONTENT_LENGTH'] = 0
     environ['SCRIPT_NAME'] = ''
-    environ['wsgi.input'] = StringIO(content)
+    environ['SERVER_NAME'] = socket.getfqdn()
+    environ['SERVER_PORT'] = str(port)
+    environ['wsgi.version'] = (1, 0)
+    environ['wsgi.multithread'] = False
+    environ['wsgi.multiprocess'] = False
+    environ['wsgi.run_once'] = False
+    environ['wsgi.url_scheme'] = 'http'
+    environ['HTTP_COOKIE'] = headers['cookie'] if 'cookie' in headers.keys() else ''
 
+    content = ''
     if request_method == 'POST':
+        while len(content) < int(headers['Content-Length']):
+            content += conn.recv(1)
         environ['CONTENT_TYPE'] = headers['Content-Type']
         environ['CONTENT_LENGTH'] = headers['Content-Length']
- 	environ['wsgi.input'] = cgi.FieldStorage(fp=StringIO(content), headers=headers.dict, environ=environ)
+    
+    environ['wsgi.input'] = StringIO(content)
 
     def start_response(status, response_headers):
         conn.send('HTTP/1.0 ')
@@ -57,32 +60,49 @@ def handle_connection(conn):
             conn.send(key + ': ' + header + '\r\n')
         conn.send('\r\n')
 
-    application = app.make_app()
-    response_html = application(environ, start_response)
-#    conn.send(response_html)
-    for html in response_html:
-        conn.send(html)
+    validator_app = validator(app)
+    response_html = app(environ, start_response)
+    for data in response_html:
+        conn.send(data)
     conn.close()
 
+def choose_app(app_name):
+    if app_name == 'altdemo':
+        p = create_publisher()
+        return quixote.get_wsgi_app()
+    elif app_name == 'image':
+        imageapp.setup()
+        p = imageapp.create_publisher()
+        return quixote.get_wsgi_app()
+        pass
+    elif app_name == 'myapp':
+        return app.make_app()
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-A', '--app', help='choose a app to run (image,altdemo,myapp)', default='myapp')
+    parser.add_argument('-p', '--port', type=int, help='choose a port (default is random)', default=random.randint(8000,9999))
+    args = parser.parse_args()
+    app = choose_app(args.app)
+
     s = socket.socket()         # Create a socket object
     host = socket.getfqdn() # Get local machine name
-    port = random.randint(8000, 9999)
+    port = args.port
     s.bind((host, port))        # Bind to the port
 
     print 'Starting server on', host, port
     print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
     s.listen(5)                 # Now wait for client connection.
-    
+
 
     print 'Entering infinite loop; hit CTRL-C to exit'
     while True:
-        # Establish connection with client.    
+        # Establish connection with client.
         c, (client_host, client_port) = s.accept()
         print 'Got connection from', client_host, client_port
 
-        handle_connection(c)
+        handle_connection(c, port, app)
 
 if __name__ == '__main__':
    main()
