74753d6c04f14f4c6ccf33d96cee402eaccc45c2
beckhamer <ft1983@gmail.com>
2014-04-20 02:47:09 -0400

    Update run-imageapp.py

--- run-imageapp.py
+++ run-imageapp.py
@@ -17,13 +17,8 @@ wsgi_app = quixote.get_wsgi_app()
 from wsgiref.simple_server import make_server
 
 host = socket.getfqdn() # Get local machine name
-<<<<<<< HEAD
 #port = random.randint(8000, 9999)
 port = 8000
-=======
-port = random.randint(8000, 9999)
-#port = 8000
->>>>>>> b9f9163fb84f8bf3d52e3372935d9d78d0c2c52e
 httpd = make_server('', port, wsgi_app)
 print "Serving at http://%s:%d/..." % (host, port,)
 
