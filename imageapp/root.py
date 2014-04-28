5e2af7dd91c5da501986db0bb669c0f355dcd83d
beckhamer <ft1983@gmail.com>
2014-04-28 15:36:37 -0400

    HW8 and Project#4

--- imageapp/root.py
+++ imageapp/root.py
@@ -20,6 +20,13 @@ class RootDirectory(Directory):
         print request.form.keys()
 
         the_file = request.form['file']
+        fileFormat = the_file.base_filename.split('.')[1].lower()
+        if fileFormat == 'tiff' or fileFormat == 'tif':
+            fileFormat = 'tiff'
+        elif fileFormat == 'jpeg' or fileFormat == 'jpg':
+            fileFormat = 'jpg'
+			
+	print 'received file of format: ' + fileFormat
         print dir(the_file)
         print 'received file with name:', the_file.base_filename
         data = the_file.read(int(1e9))
@@ -36,6 +43,6 @@ class RootDirectory(Directory):
     @export(name='image_raw')
     def image_raw(self):
         response = quixote.get_response()
-        response.set_content_type('image/png')
         img = image.get_latest_image()
+        response.set_content_type('image/%s' % img[1])
         return img
